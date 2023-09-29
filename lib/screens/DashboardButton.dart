87c2d5551801e7a1818ea6b23395f97baf7972a2
Rodrigo Garcia <rodgarcia@bi.com>
2023-09-28 19:11:38 -0600

    Coding with T, Section 2.0 - 2.3 Images and Icons

--- lib/screens/DashboardButton.dart
+++ lib/screens/DashboardButton.dart
@@ -1,6 +1,8 @@
 //Always need to import this for all the classes we create
+import 'dart:ffi';
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DashboardButton extends StatelessWidget {
   const DashboardButton({super.key});
@@ -9,18 +11,34 @@ class DashboardButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text("Dashboard", style: TextStyle(color: Colors.black)),
+          title: const Text("Dashboard",
+              style: TextStyle(
+                color: Colors.black,
+              )),
           backgroundColor: Colors.pink),
       backgroundColor: Colors.amberAccent,
+      /*body: const Center(
+        child:Image(
+            image: AssetImage("images/metroid.png")*/
       body: Center(
-          child: Text("My Random Value Is: ${getNumber()}"),
+        child: IconButton(
+          icon: Icon(
+            FontAwesomeIcons.wallet,
+          ),
+          onPressed: (){},
+          iconSize: 100,
+          color: Colors.blue,
+          hoverColor: Colors.red,
+          highlightColor: Colors.pink,
+          splashColor: Colors.black,
+        ),
       ),
     );
   }
 }
 
 
-int getNumber()
-{
+
+int getNumber() {
   return Random().nextInt(100);
 }
