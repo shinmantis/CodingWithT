08d0858d8c42532e94f1723184a2180f71f8a34c
Rodrigo Garcia <rodgarcia@bi.com>
2023-10-06 17:43:56 -0600

    Coding with T, Section 2.9A Static ListView.  More useful links in Main

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -29,25 +29,29 @@ class Home extends StatelessWidget {
         ),
       ),
       body: Container(
-        color: Colors.amber,
+        //color: Colors.amber,
         child:  ListView(
+          itemExtent: 100.0, //predetermines the size of each cell rather than letting the children determine
+          reverse: false, //Makes the listview populate from the bottom so it scrolls upward instead of downward
+          scrollDirection: Axis.vertical, //allows you to scroll in portrait/landscape mode, doesn't automatically change the text direction
           children: [
             ListTile(
               tileColor: Colors.black,
-              leading: Icon(Icons.alarm_on_sharp),
+              leading: CircleAvatar(child: Icon(Icons.alarm_on_sharp,), backgroundColor: Colors.blue, foregroundColor: Colors.red,),
               title: Text("Sales"),
               subtitle: Text("Sales Of The Week"),
               trailing: Icon(Icons.abc),
-              textColor: Colors.white,
+              textColor: Colors.grey.shade50,
               iconColor: Colors.red,
               hoverColor: Colors.purple,
               selectedColor: Colors.red,
-              selectedTileColor: Colors.grey,
+              selectedTileColor: Colors.grey.shade900,
               onLongPress: (){},
               onTap: (){},
 
             ),
             ListTile(
+              tileColor: Colors.blueGrey,
               leading: Icon(Icons.airplane_ticket),
               title: Text("Sales"),
               subtitle: Text("Sales Of The Week"),
@@ -62,6 +66,7 @@ class Home extends StatelessWidget {
 
             ),
             ListTile(
+              tileColor: Colors.amber,
               leading: Icon(Icons.car_crash),
               title: Text("Sales"),
               subtitle: Text("Sales Of The Week"),
@@ -77,6 +82,7 @@ class Home extends StatelessWidget {
             ),
             ListTile(
               leading: Icon(Icons.food_bank),
+              tileColor: Colors.deepPurpleAccent,
               title: Text("Sales"),
               subtitle: Text("Sales Of The Week"),
               trailing: Icon(Icons.abc),
