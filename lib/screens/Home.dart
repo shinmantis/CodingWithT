ab32e0ccde6993a22119af0dd8c64ae968b08622
Rodrigo Garcia <rodgarcia@bi.com>
2023-10-08 15:50:08 -0600

    Coding with T, Section 2.12 Stack Layout Widgets in Flutter With Cards Pt2

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -14,26 +14,78 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Stack"),
-          centerTitle: true,
-          backgroundColor: Colors.black38,
+      appBar: AppBar(
+        title: Text("Stack"),
+        centerTitle: true,
+        backgroundColor: Colors.black38,
+      ),
+      backgroundColor: Colors.grey.shade800,
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          constraints: const BoxConstraints.expand(width: 330, height: 450),
+          decoration: const BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.white24,
+                  offset: Offset(0, 2),
+                  spreadRadius: 5,
+                  blurRadius: 10,
+                ),
+              ],
+              image: DecorationImage(
+                image: AssetImage('images/card-1.jpg'),
+                //Don't forget to add the image file to pubpec.yaml
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular((10.0)))),
+
+          //CHILD
+          child: Stack(
+            children: [
+              Text(
+                "Editor's Choice",
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 18,
+                ),
+              ),
+              Positioned(
+                top:20.0,
+                child: Text(
+                  "The Art of Making Coffee",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 0,
+                bottom:20,
+                child: Text(
+                  "Learn to make the perfect coffee",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: Text(
+                  "Coding with T",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-        backgroundColor: Colors.grey.shade800,
-        body: Center(
-            child: Stack(
-          textDirection: TextDirection.rtl,
-          alignment: Alignment.center,
-          children: [
-            Container(width: 300, height: 200, color: Colors.green),
-            Positioned(
-              bottom: 0,
-              child: Container(width: 150, height: 100, color: Colors.red),
-            ),
-            Positioned(
-                right: 40,
-                child: Container(width: 75, height: 50, color: Colors.blue)),
-          ],
-        )));
+      ),
+    );
   }
 }
